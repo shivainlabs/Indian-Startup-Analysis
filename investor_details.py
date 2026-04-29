ab75f706f98af1b0b25b2375fe38231c2809e906
shivainlabs <shiva.in.labs@gmail.com>
2026-04-29 10:57:46 +0530

    Update investor_details -> sectors, round, city and similar investor

--- investor_details.py
+++ investor_details.py
@@ -25,34 +25,47 @@ def load_investor_details(df,investor):
     with col2:
         
         # Sector - wise Investment
-        vertical_series = df[df["investors"].str.contains(investor)].groupby("vertical")["amount"].sum()
-
         st.subheader("Sectors invested in")
-        fig1, ax1 = plt.subplots()
-        ax1.pie(vertical_series,labels=vertical_series.index,autopct="%1.1f%%") #type: ignore
-        st.pyplot(fig1)
+        vertical_series = df[df["investors"].str.contains(investor)].groupby("vertical")["amount"].sum()
+        vertical_series = vertical_series[vertical_series>0]
+        
+        if vertical_series.empty:
+            st.warning("No sector data available for this investor")
+        else:
+            fig1, ax1 = plt.subplots()
+            ax1.pie(vertical_series,labels=vertical_series.index,autopct="%1.1f%%") #type: ignore
+            st.pyplot(fig1)
         
     col1, col2 = st.columns(2)
     
     with col1:
         # Stage(round) wise Investment
+        st.subheader("Round-wise Investing")
         stage_series = df[df["investors"].str.contains(investor)].groupby("round")["amount"].sum()
         
-        st.subheader("Round-wise Investing")
-        fig3, ax3 = plt.subplots()
-        ax3.pie(stage_series,labels=stage_series.index,autopct="%1.1f%%") #type: ignore
-        st.pyplot(fig3)
+        stage_series = stage_series[stage_series > 0]
+        if stage_series.empty:
+            st.warning("No round-wise data available")
+            
+        else:
+            fig3, ax3 = plt.subplots()
+            ax3.pie(stage_series,labels=stage_series.index,autopct="%1.1f%%") #type: ignore
+            st.pyplot(fig3)
         
     with col2:
         
         # City-wise Investment
         
-        city_series = df[df["investors"].str.contains(investor)].groupby("city")["amount"].sum()
-        
         st.subheader("City-wise Investing")
-        fig3, ax3 = plt.subplots()
-        ax3.pie(city_series,labels=city_series.index,autopct="%1.1f%%") #type: ignore
-        st.pyplot(fig3)
+        city_series = df[df["investors"].str.contains(investor)].groupby("city")["amount"].sum()
+        city_series = city_series[city_series>0]
+        if city_series.empty:
+            st.warning("No city-wise data available")
+
+        else:
+            fig3, ax3 = plt.subplots()
+            ax3.pie(city_series,labels=city_series.index,autopct="%1.1f%%") #type: ignore
+            st.pyplot(fig3)
     
     col1, col2 = st.columns(2)
 
@@ -60,10 +73,14 @@ def load_investor_details(df,investor):
         # Year wise Investment    
         year_series = df[df["investors"].str.contains(investor)].groupby("year")["amount"].sum()
         st.subheader("YoY Investment")
-        fig2, ax2 = plt.subplots()
-        ax2.plot(year_series.index,year_series.values) #type: ignore
-        st.pyplot(fig2)
-    
+        if len(year_series) < 2:
+            st.warning("Not enough data for YoY trend")
+            st.write(year_series)
+        else:
+            fig2, ax2 = plt.subplots()
+            ax2.plot(year_series.index,year_series.values) #type: ignore
+            st.pyplot(fig2)
+        
     with col2:
         # Similar Investment
         st.subheader("Similar Investor")
@@ -75,8 +92,10 @@ def load_investor_details(df,investor):
             total = df[df["investors"].str.contains(x,regex=False)]["amount"].sum()
             rows.append([x,total])
             
-
-        similar_investor_df = pd.DataFrame(rows)
-        list_investor_df = pd.DataFrame(similar_investor_df.sort_values(by=1,ascending=False)[0].unique()[:5]) #type: ignore
-        list_investor_df.rename(columns = {0:"Similar Investor"},inplace=True)
-        st.dataframe(list_investor_df)
+        if len(rows)==0:
+            st.warning("No similar Investor exist")
+        else:
+            similar_investor_df = pd.DataFrame(rows)
+            list_investor_df = pd.DataFrame(similar_investor_df.sort_values(by=1,ascending=False)[0].unique()[:5]) #type: ignore
+            list_investor_df.rename(columns = {0:"Similar Investor"},inplace=True)
+            st.dataframe(list_investor_df)
